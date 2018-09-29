424520e607f3b55394badc3fdd0314d119a78106
gabrielfeba <gabrielfeba@outlook.com>
2018-09-29 01:46:35 -0300

    adicionar desenho do trajeto ternário e quartenário e alguns reparos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-let compassos = document.querySelectorAll(".compassos");
-let bpm = document.querySelector("input[type='number']");
-let iniciar = document.querySelector("section button");
-let section = document.querySelector("section");
-let container = document.querySelector("#container");
-let containerDiv = document.querySelector("#container div");
+const compassos = document.querySelectorAll(".compassos");
+const bpm = document.querySelector("input[type='number']");
+const iniciar = document.querySelector("section button");
+const section = document.querySelector("section");
+const container = document.querySelector("#container");
+const containerDiv = document.querySelector("#container div");
+const divDoCompasso = document.querySelector("body>div");
+const linhaDesenho = document.querySelectorAll(".linhaDesenho");
 
 function start() {
     let tipoDoCompasso;
@@ -12,9 +14,21 @@ function start() {
             tipoDoCompasso = Number(compasso) + 2;
         }
     }
+    if(tipoDoCompasso === 4) {
+        linhaDesenho[1].style.transform = "rotate(90deg)";
+        linhaDesenho[2].style.transform = "translate(-20vh, 20vh) rotate(-45deg)";
+        linhaDesenho[2].style.height = "57vh";
+        linhaDesenho[3].style.transform = "translate(20vh, -20vh) rotate(-45deg)";
+        linhaDesenho[3].style.height = "57vh";
+    } else {
+        linhaDesenho[2].style.transform = "translate(20vh, 20vh) rotate(45deg)";
+        linhaDesenho[2].style.height = "57vh";
+        linhaDesenho[3].style.transform = "translate(20vh, -20vh) rotate(-45deg)";
+        linhaDesenho[3].style.height = "57vh";
+    }
     let tempoDoCompasso = (60/bpm.value) * tipoDoCompasso;
     section.style.display = "none";
-    container.style.display = "block";
+    divDoCompasso.style.display = "flex";
     containerDiv.style.animation = `2s levare ease-in, ${tempoDoCompasso}s 2s compasso${tipoDoCompasso} linear infinite`;
 }
 
